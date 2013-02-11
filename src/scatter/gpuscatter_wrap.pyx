e5ba5f5e7be045390e7561d4eae2b53f2c614d84
tjlane <thomas.joseph.lane@gmail.com>
2013-02-10 20:20:42 -0800

    made cpuscatter changes to gpuscatter

--- src/scatter/gpuscatter_wrap.pyx
+++ src/scatter/gpuscatter_wrap.pyx
@@ -42,7 +42,7 @@ cdef extern from "gpuscatter.hh":
                      float* h_rx_,
                      float* h_ry_,
                      float* h_rz_,
-                     np.int32_t*   h_id_,
+                     int*   h_id_,
                      int    nCM_,
                      float* h_cm_,
                      int    nRot_,
@@ -136,9 +136,12 @@ def simulate(n_molecules, qxyz, rxyz, atomic_numbers, poisson_parameter=0.0,
     # get the Cromer-Mann parameters
     py_cromermann, py_aid = get_cromermann_parameters(atomic_numbers)
     cdef np.ndarray[ndim=1, dtype=np.float32_t] c_cromermann
-    cdef np.ndarray[ndim=1, dtype=np.int32_t] c_aid
     c_cromermann =  np.ascontiguousarray(py_cromermann, dtype=np.float32)
-    c_aid =  np.ascontiguousarray(py_aid, dtype=np.int32)
+    
+    # NOTE ON int TYPES : For some reason the numpy int types don't seem to be
+    # as robust as floats/doubles. Not sure why. The below "memoryview" method
+    # seems to work, however...
+    cdef int[::1] c_aid = np.ascontiguousarray(py_aid, dtype=np.int32) # memory-view contiguous "C" array
     assert c_aid.shape[0] == rxyz.shape[0]
     
     
