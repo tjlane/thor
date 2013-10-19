91c3889c8bb8d39ca15cfbaa6983e26922d5330d
Jonas Sellberg <sellberg@slac.stanford.edu>
2013-10-19 15:50:58 +0200

    added some comments to solid angle implementation and cleaned up header file so it is understandable from the header to figure out what the functions do (as it should)

--- src/misc/solidangle.cpp
+++ src/misc/solidangle.cpp
@@ -1,5 +1,6 @@
-// solidangle.cpp
 /*
+ *  solidangle.cpp
+ *	--------------
  * Solid angle corrections for pixel array detectors.
  *
  * Written as part of Cheetah: Jonas Sellberg <sellberg@slac.stanford.edu>
@@ -18,32 +19,55 @@
 #include <cmath>
 #include <iostream>
 
+#include "solidangle.hh"
 
+
+/* 
+ * Azimuthally symmetrical correction. Fast approximation to 
+ * `rigorousSolidAngleCorrection`. See [2].
+ * 
+ * Parameters
+ * ----------
+ * num_pixels :        the number of pixels
+ * theta :             scattering angle for each pixel (sometimes called 2-theta)
+ * correction_factor : multiply this by intensities to correct
+ *
+ * Implementation notes
+ * --------------------
+ * Currently excludes the constant_factor:
+ * double constant_factor = pixel_size * pixel_size / (z_dist * z_dist)
+ * which can be viewed as the zeroth correction to the solid angle (obtained if theta = 0)
+ * and means the scattering will still be in its original scale after correction
+ */
 void fastSAC(int num_pixels, double * theta, double * correction_factor) {
-	/* 
-	 * Azimuthally symmetrical correction. Fast approximation to 
-	 * `rigorousSolidAngleCorrection`. See [2].
-	 * 
-	 * Parameters
-	 * ----------
-	 * num_pixels :        the number of pixels
-	 * theta :             scattering angle for each pixel (sometimes called 2-theta)
-	 * correction_factor : multiply this by intensities to correct
-	 */
 			
 	for (int i = 0; i < num_pixels; i++) {
+		// JAS: note that this excludes the constant_factor:
+		//      double constant_factor = pixel_size * pixel_size / (z_dist * z_dist)
+		//      which can be viewed as the zeroth correction to the solid angle (obtained if theta = 0)
+		//      and means the scattering will still be in its original scale after correction
 		correction_factor[i] = 1.0 / cos(theta[i])*cos(theta[i])*cos(theta[i]);
 	}
 }
 
+
+
+/*
+ * Compute the solid angle of a single pixel, given the corner coordinates.
+ *
+ * Parameters
+ * ----------
+ * ccords :            corner coordinates for a pixel,
+ *					   where the first index specifies the corner (currently clock-wise from 0 = upper left corner)
+ *					   and the second index specifies direction in Cartesian coordinates (0 = X, 1 = Y, 2 = Z)
+ *
+ * Implementation notes
+ * --------------------
+ * the total solid angle of a pixel is split up to calculating the solid angle for two plane triangles (see [1]),
+ * which means that ccords should be specified so that ccords[1] and ccords[3] are DIAGONAL corners, 
+ * and ccords[0] and ccords[2] are the two corners that merges the two plane triangles.
+ */
 double pixelSolidAngle(double ccords[4][3]) {
-    /*
-     * Compute the solid angle of a single pixel, given the corner coordinates.
-     *
-     * Parameters
-     * ----------
-     * ccords : 
-     */
         
 	double determinant;
 	double denominator;
@@ -119,7 +143,24 @@ double pixelSolidAngle(double ccords[4][3]) {
     
     return total_solid_angle;
 }
-		
+
+
+
+/* 
+ * Rigorous solid angle correction, for a single element of a BasisGrid. See
+ * the Odin documentation for how this representation works. See also the
+ * rigorousExplicitSAC() function below for computing the SAC on an
+ * explicit represenatation of the detector. See [1].
+ * 
+ * Parameters
+ * ----------
+ * num_pixels_s : number of px on grid in the 's' direction
+ * num_pixels_f : number of px on grid in the 'f' direction
+ * s : slow scan vector for grid
+ * f : fast scan vector for grid
+ * p : data origin for grid
+ * correction_factor (OUTPUT) : the corrections get put here
+ */
 void rigorousGridSAC(int num_pixels_s,
                      int num_pixels_f,
                      // double pixel_size,
@@ -127,27 +168,6 @@ void rigorousGridSAC(int num_pixels_s,
                      double * f,
                      double * p,
                      double * correction_factor) {
-    /* 
-     * Rigorous solid angle correction, for a single element of a BasisGrid. See
-     * the Odin documentation for how this representation works. See also the
-     * rigorousExplicitSAC() function below for computing the SAC on an
-     * explicit represenatation of the detector. See [1].
-     * 
-     * Parameters
-     * ----------
-     * num_pixels_s : number of px on grid in the 's' direction
-     * num_pixels_f : number of px on grid in the 'f' direction
-     * s : slow scan vector for grid
-     * f : fast scan vector for grid
-     * p : data origin for grid
-     * correction_factor (OUTPUT) : the corrections get put here
-     */
-     
-    // this constantFactor is essentially a conversion to the units of 
-    // solid angle -- it is approximate at best, but shouldn't really matter
-    // unless one is trying to count photons, or the like
-    // double constantFactor; 
-    // constant_factor = pixel_size * pixel_size / (z_dist * z_dist)
  
     // for each pixel...
     // OMP possible here
@@ -186,23 +206,47 @@ void rigorousGridSAC(int num_pixels_s,
             corner_coordinates[2][j] = v[j] + d2[j]; // lower right corner
             corner_coordinates[3][j] = v[j]	- d1[j]; // lower left corner
 	    }
-	
+		
+		/* double constant_factor = pixel_size * pixel_size / (z_dist * z_dist)
+		 *
+		 * the constant_factor is essentially a conversion to the units of 
+		 * solid angle -- it can be viewed as the zeroth correction for the
+		 * azimuthally symmetric case (obtained if theta = 0).
+		 * if one wants to keep the original scale the scattering was recorded
+		 * in (e.g. ADU/pixel), one can divide by this constant_factor and still
+		 * obtain the correct q-dependence for the scattering
+		 */		
+		
+		// JAS: note that the statement below is no longer in use!
+		// we keep the absolute solid angle scale when pixelSolidAngle
+		// is NOT divided by constant_factor!
+		//
     	// remove constant term to only get theta/phi dependent part of 
     	// solid angle correction for 2D pattern
         correction_factor[i] = pixelSolidAngle(corner_coordinates);  // / constant_factor;
     }
 }
 
+
+
+/*
+ * Compute the solid angle correction from an explicit xyz representation
+ * of each pixel
+ */
 void rigorousExplicitSAC(double * pixel_xyz,
                          double * correction_factor) {
-    /*
-     * Compute the solid angle correction from an explicit xyz representation
-     * of each pixel
-     */
                          
     // not implemented yet...
     // TJL to JAS : we can probably just cp what you had in cheetah, but that
     // involves the approximation that detector rotation is negligable, right?
+	
+	// JAS: should be possible, note that the azimuthally symmetric correction
+	// does assume the detector is perfectly aligned in the vertical/horizontal
+	// plane perpendicular to the x-ray beam (i.e. the x-ray beam travels along the normal of the detector plane),
+	// but the rigorous correction does not! It rigorously calculates the solid
+	// angle of a (in this case of two) plane triangle(s) as seen from the origin,
+	// so given the origin is set to the scattering source it does still work for
+	// detector rotations!
     
 }
 
--- src/misc/solidangle.hh
+++ src/misc/solidangle.hh
@@ -1,9 +1,83 @@
-// solidangle.hh
-// header for solidangle.cpp
+/*
+ *  solidangle.hh
+ *	-------------
+ * Solid angle corrections for pixel array detectors.
+ *
+ * Written as part of Cheetah: Jonas Sellberg <sellberg@slac.stanford.edu>
+ * Modified for odin:          TJ Lane <tjlane@stanford.edu>
+ *
+ * Citations
+ * ---------
+ * [1] van Oosterom, A. & Strackee, J. The solid angle of a plane triangle. IEEE
+ * Trans Biomed Eng 30, 125–126 (1983).
+ * [2] Sellberg and Loh. See documentation.
+ *
+ */
 
+#ifndef _solidangle_hh
+#define _solidangle_hh
+
+/* 
+ * Azimuthally symmetrical correction. Fast approximation to 
+ * `rigorousSolidAngleCorrection`. See [2].
+ * 
+ * Parameters
+ * ----------
+ * num_pixels :        the number of pixels
+ * theta :             scattering angle for each pixel (sometimes called 2-theta)
+ * correction_factor : multiply this by intensities to correct
+ *
+ * Implementation notes
+ * --------------------
+ * Currently excludes the constant_factor:
+ * double constant_factor = pixel_size * pixel_size / (z_dist * z_dist)
+ * which can be viewed as the zeroth correction to the solid angle (obtained if theta = 0)
+ * and means the scattering will still be in its original scale after correction
+ */
 void fastSAC(int num_pixels, double * theta, double * correction_factor);
 
+
+/*
+ * Compute the solid angle of a single pixel, given the corner coordinates.
+ *
+ * Parameters
+ * ----------
+ * ccords :            corner coordinates for a pixel,
+ *					   where the first index specifies the corner (currently clock-wise from 0 = upper left corner)
+ *					   and the second index specifies direction in Cartesian coordinates (0 = X, 1 = Y, 2 = Z)
+ *
+ * Implementation notes
+ * --------------------
+ * the total solid angle of a pixel is split up to calculating the solid angle for two plane triangles (see [1]),
+ * which means that ccords should be specified so that ccords[1] and ccords[3] are DIAGONAL corners, 
+ * and ccords[0] and ccords[2] are the two corners that merges the two plane triangles.
+ */
+double pixelSolidAngle(double ccords[4][3]);
+
+
+/* 
+ * Rigorous solid angle correction, for a single element of a BasisGrid. See
+ * the Odin documentation for how this representation works. See also the
+ * rigorousExplicitSAC() function below for computing the SAC on an
+ * explicit represenatation of the detector. See [1].
+ * 
+ * Parameters
+ * ----------
+ * num_pixels_s : number of px on grid in the 's' direction
+ * num_pixels_f : number of px on grid in the 'f' direction
+ * s : slow scan vector for grid
+ * f : fast scan vector for grid
+ * p : data origin for grid
+ * correction_factor (OUTPUT) : the corrections get put here
+ */
 void rigorousGridSAC(int num_pixels_s, int num_pixels_f, double * s,
     double * f, double * p, double * correction_factor);
-                     
-void rigorousExplicitSAC(double * pixel_xyz, double * correction_factor);
+
+
+/*
+ * Compute the solid angle correction from an explicit xyz representation
+ * of each pixel
+ */
+void rigorousExplicitSAC(double * pixel_xyz, double * correction_factor);'
+
+#endif
