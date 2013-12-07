276ce09ab6acc2d83e4ed5951c77032946efa822
tjlane <thomas.joseph.lane@gmail.com>
2013-12-06 16:13:27 -0800

    added mask method to Shotset

--- test/xray/test_xray.py
+++ test/xray/test_xray.py
@@ -307,6 +307,12 @@ class TestShotset(object):
     # missing test: num_phi_to_spacing
 
     # missing test: test_assemble (ok for now)
+    
+    def test_augment_mask(self):
+        # smoke test only (really)
+        n_masked = np.sum(self.shot.mask)
+        self.shot.augment_mask(first_moment_cutoff=1.0, second_moment_cutoff=1.0)
+        assert np.sum(self.shot.mask) > n_masked # make sure at least one got masked
 
     def test_polar_grid(self):
         pg = self.shot.polar_grid([1.0], 360)
