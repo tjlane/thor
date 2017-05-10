c8de7ba2f65222dafbb1e0aa6ec06e6e1d7b440f
tjlane <thomas.joseph.lane@gmail.com>
2017-05-09 17:56:15 -0700

    added vMF distribution

--- src/python/math2.py
+++ src/python/math2.py
@@ -287,6 +287,81 @@ def rand_rot(rands = None):
     return RU
 
 
+def sample_vMF(mu, kappa, num_samples):
+    """
+    Generate num_samples N-dimensional samples from von Mises Fisher
+    distribution around center mu in R^N with concentration kappa.
+    
+    Parameters
+    ----------
+    mu: float, np.ndarray
+        The vMF mean parameter(s). Pass an array of length N to sample the vMF
+        distribution in R^N.
+    
+    kappa: float
+        The vMF concentration/shape parameter
+    
+    num_samples : int
+        The number of samples to draw
+    
+    Returns
+    -------
+    samples : np.ndarray
+        A num_samples x len(mu)
+
+    Reference
+    ---------
+    ..[1] https://raw.githubusercontent.com/clara-labs/spherecluster/develop/spherecluster/util.pyhttps://raw.githubusercontent.com/clara-labs/spherecluster/develop/spherecluster/util.py
+    ..[2] http://stats.stackexchange.com/questions/156729/sampling-from-von-mises-fisher-distribution-in-python
+    ..[3] https://www.mitsuba-renderer.org/~wenzel/files/vmf.pdf
+    ..[4] http://www.stat.pitt.edu/sungkyu/software/randvonMisesFisher3.pdf
+    """
+    
+    dim = len(mu)
+    result = np.zeros((num_samples, dim))
+    
+    for nn in range(num_samples):
+        # sample offset from center (on sphere) with spread kappa
+        w = _sample_weight(kappa, dim)
+
+        # sample a point v on the unit sphere that's orthogonal to mu
+        v = _sample_orthonormal_to(mu)
+
+        # compute new point
+        result[nn, :] = v * np.sqrt(1. - w**2) + w * mu
+
+    return result
+
+
+def _sample_weight(kappa, dim):
+    """
+    Rejection sampling scheme for sampling distance from center on
+    surface of the sphere.
+    """
+    
+    dim = dim - 1 # since S^{n-1}
+    b = dim / (np.sqrt(4. * kappa**2 + dim**2) + 2 * kappa)
+    x = (1. - b) / (1. + b)
+    c = kappa * x + dim * np.log(1 - x**2)
+
+    while True:
+        z = np.random.beta(dim / 2., dim / 2.)
+        w = (1. - (1. + b) * z) / (1. - (1. - b) * z)
+        u = np.random.uniform(low=0, high=1)
+        if kappa * w + dim * np.log(1. - x * w) - c >= np.log(u):
+            return w
+
+
+def _sample_orthonormal_to(mu):
+    """
+    Sample point on sphere orthogonal to mu.
+    """
+    v = np.random.randn(mu.shape[0])
+    proj_mu_v = mu * np.dot(mu, v) / np.linalg.norm(mu)
+    orthto = v - proj_mu_v
+    return orthto / np.linalg.norm(orthto)
+
+
 def kabsch(P, Q):
     """ 
     Employ the Kabsch algorithm to compute the rotation matrix U that when
